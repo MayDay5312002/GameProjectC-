2771ee40cb4a4e5b5a6544fb5681bb9bf673b6be
MayDay5312002 <104174851+MayDay5312002@users.noreply.github.com>
2022-04-22 13:51:08 -0400

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,14 @@
+using System;//this is a namespace.
+             //a namespace is basically a library but categorized, so no names are conflicted. 
+
+class Program {
+   public static void Main (string[] args) {
+    Console.WriteLine("Hello World");//WriteLine = println
+   }
+
+   void bark(){
+     Console.WriteLine("Hurf hurf");
+   }
+  
+}
+
